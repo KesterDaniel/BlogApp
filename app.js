bfc9a2573ffd1641309425e6de3b7d762ee0040f
KesterDaniel <kesterdan17@gmail.com>
2022-07-23 11:48:40 +0100

    added show, edit and update route also used _method-override

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require ("express")
 const bodyParser = require("body-parser")
+const methodOverride = require("method-override")
 const mongoose = require("mongoose")
 const port = process.env.PORT || 3000
 const app = express()
@@ -9,6 +10,7 @@ const app = express()
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
+app.use(methodOverride("_method"))
 
 
 //Mongoose/Model Config
@@ -28,11 +30,12 @@ const blogSchema = mongoose.Schema({
 const Blog = mongoose.model("Blog", blogSchema)
 
 
-//Restful Routes
+//ROOT ROUTE
 app.get("/", (req, res)=>{
     res.redirect("/blogs")
 })
 
+//INDEX ROUTE
 app.get("/blogs", async(req, res)=>{
     try {
         const allBlogs = await Blog.find({})
@@ -42,6 +45,56 @@ app.get("/blogs", async(req, res)=>{
     }
 })
 
+// NEW ROUTE
+app.get("/blogs/new", (req, res)=>{
+    res.render("new")
+})
+
+//CREATE ROUTE
+app.post("/blogs", async(req, res)=>{
+    const Blogdata = req.body.blog
+    try {
+         await Blog.create(Blogdata)
+         console.log("New blog created")
+         res.redirect("/blogs")
+    } catch (error) {
+        res.render("new")
+    }
+})
+//SHOW ROUTE
+app.get("/blogs/:id", async(req, res)=>{
+    const BlogId = req.params.id
+    try {
+        const chosenBlog = await Blog.findById(BlogId)
+        res.render("show", { chosenBlog })
+    } catch (error) {
+        res.redirect("/blogs")
+    }
+})
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", async(req, res)=>{
+    const damnedId = req.params.id
+    try {
+        const damnedBlog = await Blog.findById(damnedId)
+        res.render("edit", { damnedBlog })
+    } catch (error) {
+        res.redirect("/blogs")
+    }
+})
+
+//UPDATE ROUTE
+app.put("/blogs/:id", async(req, res)=>{
+    const risenId = req.params.id
+    const newdata = req.body.blog
+    try {
+        await Blog.findByIdAndUpdate(risenId, newdata)
+        res.redirect(`/blogs/${risenId}`)
+        console.log("Updated Blog!!")
+    } catch (error) {
+        res.redirect("/blogs")
+    }
+})
 
 app.listen(port, ()=>{
     console.log("Blogapp is up!!")
